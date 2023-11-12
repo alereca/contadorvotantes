86beeea9c51e6422edebba8009b8a879582b80f5
Alejandro Recalde <alejandrorecalde5@gmail.com>
2023-11-11 21:25:06 -0300

    .

--- src/components/Total.tsx
+++ src/components/Total.tsx
@@ -44,10 +44,10 @@ const TotalCount: React.FC<TotalCountProps> = ({ ids }) => {
     const percentage = Math.floor((ids.length / 350) * 100);
 
     return (
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-                <CircularProgressWithLabel value={percentage} />
-                <Typography variant="caption" component="div" color="text.secondary" ml={1}>{`${ids.length}/350`}</Typography>
-            </div>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+            <CircularProgressWithLabel value={percentage} />
+            <Typography variant="caption" component="div" color="text.secondary" ml={1}>{`${ids.length}/350`}</Typography>
+        </div>
     );
 };
 
