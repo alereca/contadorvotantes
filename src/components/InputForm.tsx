20b1b70152fdfd3dbebc3e226a09908d6cd1a092
Alejandro Recalde <alejandrorecalde5@gmail.com>
2023-11-15 00:11:10 -0300

    Refactor InputForm component to use Formik and Yup for form validation

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,39 +1,59 @@
-import React, { useState } from 'react';
+import React from 'react';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import Send from '@mui/icons-material/Send';
 import { IconButton } from '@mui/material';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 
 interface InputFormProps {
   onAddId: (id: string) => void;
 }
 
 const InputForm: React.FC<InputFormProps> = ({ onAddId }) => {
-  const [id, setId] = useState<string>('');
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setId(e.target.value);
-  };
-
-  const handleAddId = () => {
+  const handleAddId = (id: string) => {
     onAddId(id);
-    setId('');
   };
+
+  const validationSchema = Yup.object({
+    id: Yup.number()
+      .typeError('Debe ingresar un número')
+      .integer('El número debe ser entero')
+      .min(1, 'El número debe ser al menos 1')
+      .max(350, 'El número no debe ser mayor que 350'),
+  });
 
   return (
-    <Box sx={{ p: 2 }}>
-      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
-        <TextField
-          label="Ingrese numero de orden"
-          value={id}
-          onChange={handleInputChange}
-          sx={{ flexGrow: 1 }}
-        />
-        <IconButton onClick={handleAddId} sx={{ ml: 1 }}>
-          <Send />
-        </IconButton>
-      </Box>
-    </Box>
+    <Formik
+      initialValues={{ id: '' }}
+      validationSchema={validationSchema}
+      onSubmit={(values, { setSubmitting }) => {
+        console.log(values);
+        if (values.id !== '') {
+          handleAddId(values.id);
+          values.id = '';
+        }
+        setSubmitting(false);
+      }}
+    >
+      {({ errors, touched, isSubmitting }) => (
+        <Form>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 2, p: 2 }}>
+            <Field
+              name="id"
+              as={TextField}
+              label="Ingrese número de orden"
+              error={touched.id && Boolean(errors.id)}
+              helperText={touched.id && errors.id}
+              sx={{ flexGrow: 1 }}
+            />
+            <IconButton type="submit" disabled={isSubmitting} sx={{ ml: 1 }}>
+              <Send />
+            </IconButton>
+          </Box>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
